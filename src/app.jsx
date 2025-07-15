7b92fe1bf224eefb5633d9286d75e111c89d9e9f
cognitioplus <132303904+cognitioplus@users.noreply.github.com>
2025-07-15 09:56:03 +0800

    app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="app">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
